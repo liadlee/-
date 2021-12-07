58de42aee2968278ab13d7395a53a901787dff72
liadlee <liadlee@qq.com>
2021-12-07 21:30:24 +0800

    创建数据库连接对象模块

--- node/code/06综合搭建服务器/myserver_demo/app.js
+++ node/code/06综合搭建服务器/myserver_demo/app.js
@@ -8,8 +8,9 @@ const cors = require('cors');
 app.use(cors());
 //注册解析urlencoded格式数据的内置中间件  req.body
 app.use(express.urlencoded({extended:false}))
-//导入路由模块
-
+//导入并注册用户路由模块 路由前缀  /api
+const userRouter = require('./router/user')
+app.use('/api',userRouter)
 //以指定的端口号启动服务器
 app.listen(80,function(){
     console.log('running : http://127.0.0.1:80');
