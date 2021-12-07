6950fb8d85b9a41ffecafb2938293bf0f3f338a3
liadlee <liadlee@qq.com>
2021-12-07 21:49:20 +0800

    自定义中间件，封装res.send()优化对错误的响应，挂载res.cc()方法

--- node/code/06综合搭建服务器/myserver_demo/app.js
+++ node/code/06综合搭建服务器/myserver_demo/app.js
@@ -8,6 +8,20 @@ const cors = require('cors');
 app.use(cors());
 //注册解析urlencoded格式数据的内置中间件  req.body
 app.use(express.urlencoded({extended:false}))
+
+//自定义中间件，封装res.send()对错误的响应，挂载res.cc()方法
+app.use(function(req,res,next){
+    res.cc=function(err,status = 1){  //默认status=1 错误状态（严格来说应该写响应码）
+        res.send({
+            // 状态
+            status,
+            // 状态描述，判断 err 是 错误对象 还是 字符串
+            message: err instanceof Error ? err.message : err,
+          })
+        next(); //中间件固定格式，执行完毕后移交
+    }
+})
+
 //导入并注册用户路由模块 路由前缀  /api
 const userRouter = require('./router/user')
 app.use('/api',userRouter)
